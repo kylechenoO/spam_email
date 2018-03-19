ea9e40fcc26581efa4035a556fffed4ec24ca2cc
docs <kyle@hacking-linux.com>
2018-03-19 08:31:18 +0800

    svm commit

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,6 +12,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import svm
 
 
 ### features_train and features_test are the features for the training
@@ -19,12 +20,28 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
+clf = svm.SVC(kernel = 'rbf', C = 10000)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+rs_test = clf.predict(features_test)
+print "training time:", round(time()-t0, 3), "s"
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(rs_test, labels_test))
+
+print(sum(rs_test))
+# print('predict 10 = {}'.format(rs_test[10]))
+# print('predict 26 = {}'.format(rs_test[26]))
+# print('predict 50 = {}'.format(rs_test[50]))
 #########################################################
 
 
