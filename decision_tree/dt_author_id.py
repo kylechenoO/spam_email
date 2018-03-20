09d403fde7cb6f42ba550899e2800d085ec2927e
docs <kyle@hacking-linux.com>
2018-03-20 10:43:43 +0800

    decision_tree commit

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,7 +24,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pre = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
+print(len(features_train[0]))
+print(accuracy_score(pre, labels_test))
 
 #########################################################
 
